1f41cc434dd80c020f3393436f52c660a4d44791
Alex Hitchens <106935208+Alexx-H@users.noreply.github.com>
2026-01-14 13:36:00 -0500

    Enhance sketch.js with comments for better understanding
    Added comments for clarity in the Boids simulation code.

--- projects/Boids/sketch.js
+++ projects/Boids/sketch.js
@@ -1,26 +1,33 @@
-const flock = [];
-const numofBoids = 120;
 
-let alignSlider, cohesionSlider, seperationSlider;
+const flock = [];
+
+const numofBoids = 120; // Total Boids in the scene
+
+let alignSlider, cohesionSlider, seperationSlider; // sliders to tweak values
 
 function setup() {
   let canvas = createCanvas(800, 800);
   canvas.parent('sketch-container');
+
+  // Slider values
   alignSlider =createSlider(0, 5, 1, 0.1);
   cohesionSlider = createSlider(0, 5, 1, 0.1);
   seperationSlider = createSlider(0, 5, 1, 0.1);
+  
+  // Create Boids and push into the flock
   for (let i = 0; i < numofBoids; i++) {
     flock.push(new Boid());
   }
+
 }
 
 function draw() {
   background(51);
   
   for (let boid of flock) {
-    boid.edges();
-    boid.flock(flock);
-    boid.update();
-    boid.show();
+    boid.edges();  // check if boid is off the edge and teleport to other side
+    boid.flock(flock); // calculate forces on boid
+    boid.update(); // update the boid
+    boid.show(); // show the boid
   }
 }
